053ead012be57020e50a6c7ba780b0bfdeb0a1f0
Kenta Asai <asai.kenta@gmail.com>
2020-02-27 22:49:49 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'KBsMzEIpAtm7g8mZkLrwu66jsY2FGSQV';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.8788, -117.2359],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12
+	});
+
+	L.marker([32.8788, -117.2359]).addTo(map);
 }
